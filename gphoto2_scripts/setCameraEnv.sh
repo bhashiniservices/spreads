5025aaebcd7f87f282f8b547f091e486c4a6f241
Shiva Kumar H R <shivahr@gmail.com>
2020-11-03 16:10:40 +0530

    gphoto2 based scripts for image capture

---
+++ gphoto2_scripts/setCameraEnv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#Canon Reben 3Ti
+export ODD_PORT=""
+#export ODD_CAM_DIR="/store_00020001/DCIM/101CANON"
+
+#Nikon D3500
+export EVEN_PORT="usb:001,014"
+#export EVEN_CAM_DIR="/store_00010001/DCIM/101D3500"
+
+setPorts()
+{
+	for PORT in `gphoto2 --list-ports | grep "usb:" | awk '{print $1}'`
+	do
+		MANUFACTURER=`gphoto2 --get-config manufacturer --port=$PORT`
+		if [[ $MANUFACTURER =~ Canon.* ]]
+		then
+			ODD_PORT=$PORT
+			gphoto2 --port=$PORT --set-config imageformatsd=9
+		elif [[ $MANUFACTURER =~ Nikon.* ]]
+		then
+			EVEN_PORT=$PORT
+			gphoto2 --port=$PORT --set-config imagequality=3
+		fi
+	done
+
+}
+
+setPorts
